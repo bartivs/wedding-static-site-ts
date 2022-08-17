281101a76bc57094f8961aa33faa0030d145e46c
Barti Vadez <oscar.valdez@softec.com.py>
2022-08-17 13:23:52 -0400

    base 64 image

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `wedding-static-site`,
     siteUrl: `https://www.yourdomain.tld`,
+    image: `./src/images/icon.png`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
