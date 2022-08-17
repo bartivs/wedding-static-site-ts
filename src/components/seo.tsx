21a00f93299459be7f8ba2a39f6d434773f43568
Barti Vadez <oscar.valdez@softec.com.py>
2022-08-17 19:47:04 -0400

    icon

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -12,11 +12,19 @@ export const SEO: React.FC<Props> = ({ title, description, pathname, children })
     title: title || 'Sara y Barti',
     description: description || 'Invitacion de boda Sara y Barti',
   };
- 
+
   return (
     <>
       <title>{seo.title}</title>
-      <meta name="description" content={seo.description}/>
+      <meta name="description" content={seo.description} />
+      <meta
+        name="image"
+        content="https://lh3.googleusercontent.com/drive-viewer/AJc5JmT-ci_HGz2AFNCB1IBRR-rspCkLRitAroAPQgLM0ZhBOxS4xUWkZTVeKvpaOBydPjYETQbv38Q=w1920-h909"
+      />
+      <meta
+        name="og:image"
+        content="https://lh3.googleusercontent.com/drive-viewer/AJc5JmT-ci_HGz2AFNCB1IBRR-rspCkLRitAroAPQgLM0ZhBOxS4xUWkZTVeKvpaOBydPjYETQbv38Q=w1920-h909"
+      />
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>💍</text></svg>"
