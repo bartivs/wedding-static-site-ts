337dde2a72269bf78567e0c685b1c2da552cdddc
Barti Vadez <oscar.valdez@softec.com.py>
2022-08-17 19:28:31 -0400

    se agrega analitics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -40,6 +40,26 @@ const config: GatsbyConfig = {
       },
       __key: 'images',
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-VX37E4LKY9', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // Defaults to https://www.googletagmanager.com
+        },
+      },
+    },
   ],
 };
 
